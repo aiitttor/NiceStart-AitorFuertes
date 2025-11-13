9934122a90f64639dd491279b841ff4c3bc39e33
aiitttor <afa815@educa.madrid.org>
2025-11-13 09:32:28 +0100

    Se añaden fotos de fondo en splash, login y singup.

--- app/src/main/java/com/example/nicestart_aitorfuertes/Login.java
+++ app/src/main/java/com/example/nicestart_aitorfuertes/Login.java
@@ -1,8 +1,10 @@
 package com.example.nicestart_aitorfuertes;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.example.nicestart_aitorfuertes.MainActivity;
 import com.example.nicestart_aitorfuertes.R;
 
@@ -20,11 +24,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+//                .centerCrop()
+//                .circleCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.naranja)))
+                .into(mGirl);
     }
 
     public void openMain(View v){
--- app/src/main/java/com/example/nicestart_aitorfuertes/Splash.java
+++ app/src/main/java/com/example/nicestart_aitorfuertes/Splash.java
@@ -1,14 +1,20 @@
 package com.example.nicestart_aitorfuertes;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
 
@@ -18,11 +24,17 @@ public class Splash extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
         openApp();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView mSea = findViewById(R.id.backView);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.naranja)))
+//                .circleCrop()
+                .into(mSea);
     }
 
     private void openApp() {
@@ -35,7 +47,7 @@ public class Splash extends AppCompatActivity {
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
-        }, 5000);
+        }, 2000);
     }
 
 
