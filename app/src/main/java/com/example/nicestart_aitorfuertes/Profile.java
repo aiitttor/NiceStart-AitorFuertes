0bf9ea0ed8037d7a6c6a8fe99ecf6590137916d4
aiitttor <afa815@educa.madrid.org>
2025-11-13 13:57:11 +0100

    Creacion del Activity profile

---
+++ app/src/main/java/com/example/nicestart_aitorfuertes/Profile.java
@@ -0,0 +1,41 @@
+package com.example.nicestart_aitorfuertes;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+
+        ImageView mSea = findViewById(R.id.profileLogo);
+
+        Glide.with(this)
+                .load("https://toppng.com/uploads/preview/foto-carnet-11550717604z0lxjntkz4.png")
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .circleCrop()
+                .into(mSea);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
