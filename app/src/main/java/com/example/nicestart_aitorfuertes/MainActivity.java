0ba17e818b83da7a811ce52e65ac29c584d11317
aiitttor <afa815@educa.madrid.org>
2025-11-20 13:12:42 +0100

    Creacion de los menu context y menu appbar, inserción de varios iconos y

--- app/src/main/java/com/example/nicestart_aitorfuertes/MainActivity.java
+++ app/src/main/java/com/example/nicestart_aitorfuertes/MainActivity.java
@@ -1,24 +1,94 @@
 package com.example.nicestart_aitorfuertes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView mycontext = findViewById(R.id.holaMundo);
+        registerForContextMenu(mycontext);
+
+        swipeLayout = findViewById(R.id.mySwipeRefresh);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+    }
+
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            Toast toast0 = Toast.makeText(MainActivity.this, "Has refrescado la pagina!", Toast.LENGTH_LONG);
+            toast0.show();
+            swipeLayout.setRefreshing(false);
+
+//            final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
+////
+//            Snackbar snackbar = Snackbar
+//                    .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_SHORT)
+//                    .setAction("UNDO", new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+//                            snackbar1.show();
+//                        }
+//                    });
+//
+//            snackbar.show();
+//
+//            miVisorWeb.reload();
+        }
+    };
+
+            @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+//        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+//                item.getMenuInfo();
+        if (item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(this, "Item copied",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast2 = Toast.makeText(this, "Downloading item...",
+                    Toast.LENGTH_LONG);
+            toast2.show();
+        }
+        return false;
     }
 }
